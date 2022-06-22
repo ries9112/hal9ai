ffed8b69cd3c089b7b2760b010bc4efc451e3141
Fernando <fptcorrea@gmail.com>
2022-06-22 12:20:20 -0300

    build_components first draft. uses dplyr, but we should drop it later

---
+++ r/R/build_components.R
@@ -0,0 +1,14 @@
+build_components <- function(){
+  components <- jsonlite::read_json(
+    "https://raw.githubusercontent.com/hal9ai/hal9ai/main/scripts/components.json", simplifyVector = TRUE
+  ) |>
+  purrr::map_dfr(identity) |>
+  dplyr::filter(stringr::str_detect(source, "transforms")) |>
+  dplyr::group_by(name) |>
+  dplyr::mutate(
+    component_params = purrr::map(source, ~parse_txt_js(paste0("../scripts/", .x)))
+  ) |>
+  dplyr::ungroup()
+
+  usethis::use_data(components, internal = TRUE)
+}
